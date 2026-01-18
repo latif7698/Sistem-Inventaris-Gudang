8bf74be958478132a18f81658f944f31f525775b
Latip <asepabdullatip741@gmail.com>
2026-01-19 00:01:08 +0700

    Day 7: (Authentication & Login)

--- models.py
+++ models.py
@@ -9,3 +9,11 @@ class InventoryBD(Base):
     price = Column(Integer,nullable= False)
     stock = Column(Integer,nullable= False)
     description = Column(String(1000))
+
+# ---- Tambahan baru tabel user ----
+class UserDB(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key = True, index = True)
+    username = Column(String, unique=True, index = True) #unique =True, boar gak kembar
+    hashed_password = Column(String) # Kita simpan HASIL ACAKAN, bukan password asli
