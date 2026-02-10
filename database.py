59b13532788d6825c1f2762f7a891dbc5cc719af
Latip <asepabdullatip741@gmail.com>
2026-02-10 23:00:56 +0700

    Day 15: Dockerization with Dynamic DB Host

--- database.py
+++ database.py
@@ -4,9 +4,9 @@ from sqlalchemy.ext.declarative import declarative_base
 
 
 # password hrus sesuai dengan yang di PostgreSQL
-#SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin123@localhost/inventory-api"
+#SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin123@host.docker.internal/inventory-api"
 # Ganti ujungnya jadi inventory_db
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:mydonutgua123@localhost/inventory_db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:mydonutgua123@host.docker.internal/inventory_db"
 
 # mesin koneksi
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
